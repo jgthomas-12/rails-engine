da2e348191f21a9bda494904e38f4fc9baa1923f
Jesse Thomas <jgthomas3@gmail.com>
2023-07-11 15:52:46 -0600

    Feat: Adjust resources

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index]
+        resources :items, only: [:index], controller: "merchants/items"
       end
+
+      resources :items, only: [:index]
     end
   end
 end
