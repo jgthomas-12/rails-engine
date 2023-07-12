4c485cb8e71861a7a766323e9280ed6fbe3fc488
Jesse Thomas <jgthomas3@gmail.com>
2023-07-12 12:52:01 -0600

    Test: Item model associations and validations

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Item, type: :model do
-  it { should belong_to :merchant }
+  describe "associations" do
+    it { should belong_to :merchant }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:unit_price) }
+    it { should validate_numericality_of(:unit_price).is_greater_than_or_equal_to(0).allow_nil }
+    it { should validate_presence_of(:merchant_id) }
+  end
 end
