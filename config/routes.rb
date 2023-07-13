4e7aa4e4b5f4ff1e3aa9d2128f5a070ff7962cdf
Jesse Thomas <jgthomas3@gmail.com>
2023-07-13 16:44:44 -0600

    Feat: Adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get "/merchants/find", to: "merchants/search#search"
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: "merchants/items" do
         end
       end
 
+      get "/items/find_all", to: "items/search#search"
       resources :items, only: [:index, :show, :create, :update, :destroy] do
         resources :merchant, only: [:index], controller: "items/merchant"
       end
