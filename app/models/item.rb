de9fc7af21e369fbd30482e7ddac6048b8b8c8f8
Jesse Thomas <jgthomas3@gmail.com>
2023-07-14 09:46:54 -0600

    Feat: .find_items_by_min_price

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,11 @@ class Item < ApplicationRecord
   validates :name, :description, :unit_price, :merchant_id, presence: true, allow_blank: false
   validates :unit_price, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
 
-  def self.find_items(search_params)
+  def self.find_items_by_name(search_params)
     where("name ilike ?", "%#{search_params}%").order(name: :asc)
+  end
+
+  def self.find_items_by_min_price(search_params)
+    where("unit_price >= ?", search_params)
   end
 end
